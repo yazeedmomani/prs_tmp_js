bbd4ad857ef3757d51fd2d6daef82d2f67efc246
Yazeed al-Momani <yazeed.02.momani@outlook.com>
2022-11-10 12:22:59 +0300

    update random function

--- resources/scripts/scripts.js
+++ resources/scripts/scripts.js
@@ -2,7 +2,7 @@
 // INCLUDED IN TEMPLATE
 // ******************************
 //Generates Random Number (Min and Max are included)
-const randomInt = (min, max) => Math.trunc(Math.random() * (max - min + 1) + 1) + min - 1;
+const randomInt = (min, max) => Math.floor(Math.random() * (max - min + 1) + 1) + min - 1;
 
 // ******************************
 // NOT INCLUDED IN TEMPLATE
